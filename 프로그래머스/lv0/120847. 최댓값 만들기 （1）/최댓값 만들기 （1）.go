8c60226b0bde94daa6aa046996c565c71ed93de1
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-07-18 18:13:43 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 3.56 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.go
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.go
@@ -1,9 +1,22 @@
 import (
     //"fmt"
-    "sort"
+    //"sort"
 )
 func solution(numbers []int) int {
+    /*
     sort.Ints(numbers)
     //fmt.Println(numbers[len(numbers)-1])
     return numbers[len(numbers)-1] * numbers[len(numbers)-2]
+    */
+    var max int = 0
+    var second_max int = 0
+    for i := 0; i < len(numbers); i++ {
+        if numbers[i] > max {
+            second_max = max
+            max = numbers[i]
+        } else if numbers[i] > second_max{
+            second_max = numbers[i]
+        }
+    }
+    return max * second_max
 }
