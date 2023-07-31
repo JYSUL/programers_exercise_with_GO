4b9d65e641bf37826188f335cc003892db6edbd3
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-07-31 21:46:28 +0900

    [unrated] Title: 문자열 섞기, Time: 0.00 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.go
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.go
@@ -2,8 +2,7 @@ func solution(str1 string, str2 string) string {
     var answer_str string = ""
     var p *string = &answer_str
     for i := 0; i < len(str1); i++ {
-        *p += string(str1[i])
-        *p += string(str2[i])
+        *p += string(str1[i]) + string(str2[i])
     }
     return answer_str
 }
