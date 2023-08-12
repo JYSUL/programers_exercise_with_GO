806e7b92fabc268d8402b74686709c4824905746
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-12 12:51:17 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 0.00 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.go
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.go
@@ -1,10 +1,13 @@
 func solution(arr []int, queries [][]int) []int {
     for _, query := range queries {
         start, end, standard_num := query[0], query[1], query[2]
-        for i := start; i <= end; i++ {            
-            if i % standard_num == 0 {
-                arr[i] = arr[i] + 1
-            }
+        i := start / standard_num
+        if start % standard_num != 0 {
+            i++
+        }
+        i *= standard_num
+        for ; i < end+1; i += standard_num {  
+            arr[i] = arr[i] + 1            
         }
     }
     return arr
