332d3ff89001f44788efad0c79151b3d1e701387
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-14 11:58:54 +0900

    [unrated] Title: 배열 만들기 5, Time: 0.10 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.go
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.go
@@ -6,7 +6,10 @@ import (
 func solution(intStrs []string, k int, s int, l int) []int {
     var answer []int
     for _, str := range intStrs {
-        new_num, _ := strconv.Atoi(str[s:s+l])
+        new_num, err := strconv.Atoi(str[s:s+l])
+        if err != nil {
+            continue
+        }
         if new_num > k {
             answer = append(answer, new_num)
         }
