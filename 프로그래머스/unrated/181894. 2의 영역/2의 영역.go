5169492aa20e7cc9aae5a920151c0b536f15dc2e
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-14 17:29:24 +0900

    [unrated] Title: 2의 영역, Time: 0.00 ms, Memory: 6.72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.go
@@ -0,0 +1,23 @@
+func solution(arr []int) []int {
+    var flag bool = false
+    var start, end int = 0, 0
+    for i := 0; i < len(arr); i++ {
+        if arr[i] == 2 {
+            start = i
+            flag = true
+            break
+        }
+    }
+    for i := len(arr) -1; i >= 0; i-- {
+        if arr[i] == 2 {
+            end = i
+            flag = true
+            break
+        }
+    }
+    if !flag {
+        return []int{-1}
+    }
+    
+    return arr[start: end+1]
+}
