4aa52bc2c3bf2478ccb252dfce9920a8c47f8542
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-17 13:53:37 +0900

    [unrated] Title: x 사이의 개수, Time: 0.56 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.go
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.go
@@ -1,5 +1,7 @@
+import "strings"
 func solution(myString string) []int {
     var answer_slice []int 
+    /*
     var start int = 0
     for index, alp := range myString {
         if string(alp) == "x" {
@@ -8,5 +10,11 @@ func solution(myString string) []int {
         }
     }
     answer_slice = append(answer_slice, len(myString) - start)
+    */
+    for _, str := range strings.Split(myString, "x") {
+        answer_slice = append(answer_slice, len(str))
+    }
+    
     return answer_slice
+    
 }
