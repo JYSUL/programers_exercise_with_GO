d6cb53d09e9424fda51bc7ea781bf850a8f2f0e6
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-17 13:51:44 +0900

    [unrated] Title: x 사이의 개수, Time: 0.87 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.go
@@ -0,0 +1,12 @@
+func solution(myString string) []int {
+    var answer_slice []int 
+    var start int = 0
+    for index, alp := range myString {
+        if string(alp) == "x" {
+            answer_slice = append(answer_slice, index - start)
+            start = index + 1
+        }
+    }
+    answer_slice = append(answer_slice, len(myString) - start)
+    return answer_slice
+}
