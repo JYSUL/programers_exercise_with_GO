d1c87115056b81c79e41a80594b8ddaf1fa57ce5
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-20 19:10:46 +0900

    [unrated] Title: 문자열 나누기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.go
@@ -0,0 +1,25 @@
+//import "fmt"
+func solution(s string) int {
+    var answer int = 0
+    var same_num, diff_num int = 0, 0
+    var str byte = s[0]
+    for i := 0; i < len(s) - 1; i++ { // 마지막은 분리가 되든, 안되는 +1하기위해서 i < len(s) - 1
+        if str == s[i] {
+            same_num++
+        } else {
+            diff_num++
+        }
+        
+        if same_num == diff_num {
+            same_num, diff_num = 0, 0
+            if i < len(s) - 1 {
+                str = s[i+1]
+            }
+            answer++
+        }
+    }
+    //fmt.Println(string(str))
+    //마지막
+    answer++ 
+    return answer
+}
