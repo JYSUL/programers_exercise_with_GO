7e1327b91b1f88cd5caff959001d7f9c2a49aea1
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-21 16:31:09 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.15 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.go
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.go
@@ -7,11 +7,11 @@ func solution(numbers int, limit int, power int) int {
     
     for number := 1; number <= numbers; number++ {
         count := 0
-        num := int(math.Sqrt(float64(number)))
-        if num * num == number {
+        num := math.Sqrt(float64(number))
+        if num == math.Floor(num) {
             count = -1
         }
-        for i := 1; i <= num; i++ {
+        for i := 1; i <= int(num); i++ {
             if number % i == 0 {
                 count += 2
             }
