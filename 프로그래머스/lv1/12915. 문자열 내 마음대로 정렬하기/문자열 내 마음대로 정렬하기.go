302a2e919ded8f66254bbdebdcd2bda10b003977
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-27 21:31:08 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.go
@@ -0,0 +1,11 @@
+import "sort"
+func solution(strings []string, n int) []string {
+    sort.Slice(strings, func (i, j int) bool {
+        if strings[i][n] != strings[j][n] {
+            return strings[i][n] < strings[j][n]
+        } else {
+            return strings[i] < strings[j]
+        }        
+    })
+    return strings
+}
