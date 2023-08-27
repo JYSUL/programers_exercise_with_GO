5e99a6a3b040615cb262cd7f59fd0b65f6157d84
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-27 21:01:00 +0900

    [level 1] Title: 시저 암호, Time: 8.90 ms, Memory: 8.46 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.go
@@ -0,0 +1,31 @@
+import (
+    //"fmt"
+    //"strings"
+)
+func solution(s string, n int) string {
+    var answer string = ""
+    for _, str := range s {
+        if string(str) == " " {
+            answer += " "
+            continue
+        }
+        if str > 96 {
+            str += rune(n)
+            if str > 122 {
+                str -= 26
+            }
+        } else {
+            str += rune(n)
+            if str > 90 {
+                str -= 26
+            }
+        }
+        
+        answer += string(str)
+        
+    }
+    return answer
+}
+
+
+// azAZ 97 122 65 90
