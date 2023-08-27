cedf8bc696946037c8bd66d7e577420bab82121f
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-27 13:16:16 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 3.53 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.go
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.go
@@ -1,9 +1,6 @@
 func solution(price int, money int, count int) int64 {
-    var total_price int = 0
+    var total_price int = price * count * (count+1) / 2
     
-    for i := 0; i <= count; i++ {
-        total_price += price * i
-    }
     
     if total_price <= money {
         return 0
