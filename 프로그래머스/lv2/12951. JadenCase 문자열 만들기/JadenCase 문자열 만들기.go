0c7d839211d0791c83e9541e4b425d0eb64f513f
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-08-29 18:44:08 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.go
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.go
@@ -2,7 +2,7 @@ import (
     "strings"
 )
 func solution(s string) string {
-    //strs := strings.Fields(s)
+    //strs := strings.Fields(s) fields 는 공백말고 다른 특수 기호들까지도 split한다.
     strs := strings.Split(s, " ")
     for index, str := range strs {
         strs[index] = strings.Title(strings.ToLower(str))
