73420bbba9826164d90d4b4e4ef3cac2b3e71e4c
JYSUL <44855542+JYSUL@users.noreply.github.com>
2023-09-27 14:47:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.go
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.go
@@ -1,11 +1,15 @@
-import ( 
+import (
+    //"fmt"
     "strings"
 )
 func solution(s string) string {
     //strs := strings.Fields(s) fields 는 공백말고 다른 특수 기호들까지도 split한다.
+    
     strs := strings.Split(s, " ")
     for index, str := range strs {
         strs[index] = strings.Title(strings.ToLower(str))
     }
+    
     return strings.Join(strs," ")
+    //return strings.Title(s)
 }
--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=go) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.00 ms
+메모리: 4.13 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
