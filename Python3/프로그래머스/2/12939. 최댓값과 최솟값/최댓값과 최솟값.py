14bbcc4869412f8bd094b268cbb1dc3ae25260e4
JYSUL <44855542+JYSUL@users.noreply.github.com>
2025-06-12 16:27:30 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.43 MB -BaekjoonHub

--- Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,12 @@
 def solution(s):
-    answer = list(map(int,s.split()))
-    return str(min(answer)) + " " + str(max(answer))
+    list_string = []
+    for num in s.split() :
+        list_string.append(int(num))
+    
+    min_num, max_num = list_string[0], list_string[0]
+    for num in list_string :
+        if min_num > num : min_num = num
+        if max_num < num : max_num = num
+    
+    
+    return str(min_num) + " " + str(max_num)
